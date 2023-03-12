ce5a1aaed2ca5383d069b9f4d753eafb38133b9a
Moldovan Dennis <dennismoldovan32@gmail.com>
2023-03-12 14:43:23 +0200

    project done

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,11 +1,9 @@
 const jwt=require('jsonwebtoken');
- const CustomAPIError=require('../errors/custom-error')
-
+const {BadRequest}=require('../errors/bad-request');
 const login = async (req,res)=>{
     const {username,password}=req.body;
-    console.log(username,password);
     if(!username || !password){
-        throw new CustomAPIError('Please provide username and password',400);
+        throw new BadRequest('Please provide username and password');
     }
     
     const id = new Date().getDate();
@@ -15,7 +13,6 @@ const login = async (req,res)=>{
 }
 
 const dashboard = async (req,res)=>{
-    console.log(req.user);
     const luckyNumber = Math.floor(Math.random()*100)
     res.status(200).json({msg:`Hello, ${req.user.username}`,secret:`Here is your authorized data ${luckyNumber}`});
 }
--- 05-JWT-Basics/starter/middleware/auth.js
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -1,10 +1,11 @@
 const jwt=require('jsonwebtoken');
-const CustomAPIError=require('../errors/custom-error')
+const {UnauthenticatedError}=require('../errors/index');
+
 
 const authenticationMiddleware = async (req,res,next)=>{
     const authHeader = req.headers.authorization;
     if(!authHeader || !authHeader.startsWith('Bearer ')){
-        throw new CustomAPIError('No token provided',401);
+        throw new UnauthenticatedError('No token provided');
     }
     const token = authHeader.split(' ')[1];
 
@@ -14,9 +15,8 @@ const authenticationMiddleware = async (req,res,next)=>{
         req.user={id,username};
         next();
     }catch(err){
-        throw new CustomAPIError('Not authorized to this route',401);
-    } 
-
+        throw new UnauthenticatedError('Not authorized to this route');
+    }
 };
 
 module.exports = authenticationMiddleware;
